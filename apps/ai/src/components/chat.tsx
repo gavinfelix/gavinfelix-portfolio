9fe8f468f64dda221d12189527247041de520e7e
GavinFelix Sun <gavinfelixsun@gmail.com>
2025-10-10 23:06:18 +0800

    feat: add MultimodalInput component to enhance chat functionality

--- apps/ai/src/components/chat.tsx
+++ apps/ai/src/components/chat.tsx
@@ -1,11 +1,14 @@
 "use client";
 import { Messages } from "./messages";
+import { MultimodalInput } from "./mutimodal-input";
 
 export function Chat({}) {
   return (
     <>
       <div>
         <Messages />
+        --
+        <MultimodalInput />
       </div>
     </>
   );
---
+++ apps/ai/src/components/mutimodal-input.tsx
@@ -0,0 +1,11 @@
+export function MultimodalInput() {
+  return (
+    <div>
+      <input
+        className="-top-4 -left-4 pointer-events-none fixed size-0.5 opacity-0"
+        multiple
+      />
+      Send a message...
+    </div>
+  );
+}
